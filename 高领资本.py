17c57aa144ebb2b6a62196f8fecff75b78d27863
wangyandi <935415150@qq.com>
2017-11-16 00:06:41 +0800

    15号最后一次提交

---
+++ 高领资本.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Date: 2017/11/15
+
+#1mysql有哪些储存引擎，优化mysql数据库的方法有哪些
+
+'''
+ISMA：最古老的引擎
+MYISMA：mysql版本的isma引擎
+HEAP：追求速度但是需要在关机前进行保存操作
+innoDB：mysql创造了innodb同时innodb也造就了mysql，也是mysql发展起来的重要引擎，通过mysql+api的方式处理数据，mysql下支持外键和事务处理的引擎
+'''
