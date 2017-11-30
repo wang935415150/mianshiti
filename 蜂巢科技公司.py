a5d351470eb611cf27f175548e195731696b7f10
wangyandi <935415150@qq.com>
2017-11-30 23:26:48 +0800

    no message

--- 蜂巢科技公司.py
+++ 蜂巢科技公司.py
@@ -37,6 +37,8 @@ print(c)
 '''
 
 #3
+'''
+将这个列表从大到小排列
 li=[-2,1,3,-6]
 def absfull(li):
     for  i in range(len(li)-1):
@@ -45,3 +47,24 @@ def absfull(li):
                 li[j],li[j+1]=li[j+1],li[j]
     return li
 print(absfull(li))
+
+……#######################
+sort排序和sorted的区别
+a=[1,2,6,2,3,8,12,3]
+a.sort()#是直接修改了原来的列表返回值是None
+sorted(a)#不修改原来的列表，返回一个新的列表
+'''
+
+#4统计一个文本a.txt中的单词数量并且返回其中最高的是个
+dic=dict()
+with open('a.txt','r',encoding='utf-8') as f:
+    a=f.read()
+    for i in a:
+        c=a.count(i)
+        dic[i]=c
+    f=zip(dic.values(),dic.keys())
+    f=sorted(f)
+    print(f[-10:])
+
+
+
