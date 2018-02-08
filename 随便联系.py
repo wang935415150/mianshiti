8c6920d0543a767bb3ba974e349e354971caaff6
wangyandi <935415150@qq.com>
2018-02-08 21:05:47 +0800

    no message

--- 随便联系.py
+++ 随便联系.py
@@ -18,10 +18,33 @@
 #
 
 #求1-1000000的质数
-for n in range(2, 100000000000000000000):
-    for x in range(2, n+1):
-        if n % x == 0 and n/x!=1:
-            # print(n, '等于', x, '*', n // x)
-            break
-    else:
-        print(n, ' 是质数')
+# for n in range(2, 100000000000000000000):
+#     for x in range(2, n+1):
+#         if n % x == 0 and n/x!=1:
+#             # print(n, '等于', x, '*', n // x)
+#             break
+#     else:
+#         print(n, ' 是质数')
+
+def test_1():
+    func_array=[]
+    for i in range(10):
+        def func():
+            print(i)
+        func_array.append(func)
+    func_array[0]()
+    func_array[9]()
+def test_2():
+    func_array=[]
+    for i in range(10):
+        def func(i):
+            def wrap():
+                print(i)
+            return wrap
+        func_array.append(func(i))
+    func_array[0]()
+    func_array[9]()
+print('test1')
+test_1()
+print('test2')
+test_2()
--- 面试题汇总不会的.py
+++ 面试题汇总不会的.py
@@ -3,4 +3,23 @@
 # __author__ = "YGZBxia"
 # Date: 2017/12/1
 
+def fun(li):
+    if len(li) <=1 :return li
+    temp=li[len(li)//2]
+    left = [i for i in li if i < temp]
+    middel = [i for i in li if i == temp]
+    right = [i for i in li if i > temp]
+    return fun(left) + middel + fun(right)
 
+def func(li):
+    if len(li) <= 1:
+        return li
+    temp = li[len(li)//2]
+    left = [i for i in li if i < temp]
+    middel = [i for i in li if i == temp]
+    right = [i for i in li if i > temp]
+    return func(left) + middel + func(right)
+
+li = [1,2,3,4,13,55,11,23,12]
+print(func(li))
+# print(fun(li))
